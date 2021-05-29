2f49746bfa4f8e9ab6c90bc6625c26bf2cffc421
mwickramanayaka <manulwick@gmail.com>
2021-05-29 16:29:16 +0530

    testing1.xml file included

--- src/test/java/listeners/TestNGListenersTest.java
+++ src/test/java/listeners/TestNGListenersTest.java
@@ -4,41 +4,52 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
- 
+import org.testng.Assert;
 import org.testng.SkipException;
-
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestNGListenersTest {
-
 	
-
+	 WebDriver driver = null;
+	 static String expectedTitleebay ="Electronics, Cars, Fashion, Collectibles & More | eBay";
+	 static String expectedTitledaraz ="Online Shopping Sri Lanka: Clothes, Electronics & Phones | Daraz.lk";
 
 	@Test
 	public void test1() {
 		System.out.println("I am inside Test1");
-
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		 
+		driver.manage().window().maximize();
+		driver.get("https://www.daraz.lk/#");
+		
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitledaraz);
+		
+		driver.findElement(By.xpath("//*[@id=\"q\"]")).sendKeys("Mobile");
+		driver.findElement(By.xpath("//*[@id=\"topActionHeader\"]/div/div[2]/div/div[2]/form/div/div[2]/button")).sendKeys(Keys.RETURN);	
+		
+		driver.quit();
+		
 	}
 
 	@Test
 	public void test2() {
 		System.out.println("I am inside Test2");
-		
-		
-		
+
 		WebDriverManager.chromedriver().setup();
-		
-		WebDriver driver = new ChromeDriver();
+ driver = new ChromeDriver();
 	 
-	    
+		driver.manage().window().maximize();
 	    driver.get("https://www.ebay.com");
 	    
-	    driver.findElement(By.name("_nkw")).sendKeys("Mobiles");
-	    driver.findElement(By.name("hhh")).sendKeys("Mobiles");
-
+	    String actualTitle = driver.getTitle();
+	    Assert.assertEquals(actualTitle, expectedTitleebay);
 	    
+	    driver.findElement(By.name("_nkw")).sendKeys("Mobile");
 	    driver.findElement(By.id("gh-btn")).sendKeys(Keys.RETURN);
 	    
 		driver.quit();
@@ -51,4 +62,9 @@ public class TestNGListenersTest {
 		throw new SkipException("TestSkipped");
 	}
 
+	@Test
+	public void test4() {
+		System.out.println("I am inside Test4");
+		Assert.assertTrue(false);
+	}
 }
