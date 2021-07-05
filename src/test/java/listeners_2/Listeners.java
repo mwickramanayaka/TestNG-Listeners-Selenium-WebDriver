821d9f9b21f2fe86ee7fc46be5416652b6965758
mwickramanayaka <manulwick@gmail.com>
2021-07-06 03:03:15 +0530

    listener class added

--- src/test/java/listeners_2/Listeners.java
+++ src/test/java/listeners_2/Listeners.java
@@ -1,5 +1,46 @@
 package listeners_2;
 
-public class Listeners {
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listeners implements ITestListener {
+	
+	public void onTestStart(ITestResult result) {
+		System.out.println("*****Test started: " + result.getName());
+		
+	}
+	
+	public void onTestSuccess(ITestResult result) {
+		System.out.println("*****Test is sucessful: " + result.getName());
+		
+	}
+	
+	public void onTestFailure(ITestResult result) {
+		System.out.println("*****Test is Failed: " + result.getName());
+		
+		
+	}
+	
+	public void onTestSkipped(ITestResult result) {
+		System.out.println("*****Test is Skipped: " + result.getName());
+		
+	}
+	
+	public void onTestFaliedButWithinSucessPrecentage(ITestResult result) {
+	 
+		
+	}
+	
+	public void onStart(ITestContext context) {
+	
+		
+	}
+	
+	public void onFinish(ITestContext context) {
+		System.out.println("*****Test is Finished: " + context.getName());
+
+		
+	}
 
 }
